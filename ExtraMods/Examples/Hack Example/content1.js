2bcf89e7f6c1775451743d6a8f3d29a5148b6c01
Lapide <64395933+nonumbershere@users.noreply.github.com>
2020-09-16 15:55:44 -0700

    Add files via upload

---
+++ ExtraMods/Examples/Hack Example/content1.js
@@ -0,0 +1,8 @@
+var Hacks = {
+        colorFullSkin: function e() {
+            document.querySelector("#offline-resources-1x").src = "https://github.com/nonumbershere/Skinz4ChromeDino/blob/master/COLORED/ColoredTrexPack.png?raw=true"; document.querySelector("#offline-resources-2x").src = "https://github.com/nonumbershere/Skinz4ChromeDino/blob/master/COLORED/ColoredTrexPack.png?raw=true"
+    },
+    SpeedHack: function e(speed) {
+        Runner.instance_.setSpeed(speed);
+    }
+}
---
+++ ExtraMods/Examples/Hack Example/data/contents.js
@@ -0,0 +1,11 @@
+var r;
+var hack;
+(function () {
+    if (hack == 'resize') {
+        document.body.style.color = 'red';
+    }
+    if (hack == r) {
+        document.body.style.color = 'lightblue';
+    }
+});
+hack = r;
