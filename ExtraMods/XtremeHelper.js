e9b8d247754c73aa143fdc85530b8d88deb5a7c7
Lapide <64395933+nonumbershere@users.noreply.github.com>
2020-09-21 17:35:13 -0400

    Update XtremeHelper.js

--- ExtraMods/XtremeHelper.js
+++ ExtraMods/XtremeHelper.js
@@ -38,6 +38,7 @@ var brown = 'brown';
 var gray = 'gray';
 var lime = 'lime';
 // now for the HACK!
+var r = document.createElement('div');
 var Xtreme = {
  turnOffArcadeMode: function() {
  Runner.instance_.isArcadeMode = function(){}
@@ -46,14 +47,19 @@ Runner.instance_.setArcadeMode = function(){}
  freshAlert: function(title, msg){
   function p() {
    r.style.transition = '2s';
-r.style.opacity='0'
-   setTimeout(function(){r.remove()}, 1000);
+     r.style.opacity='1';
+   setTimeout(function(){r.remove(); r.style.opacity='1000'}, 1000);
 }
-var r = document.createElement('div');
-r.innerHTML='<div style="-webkit-font-smoothing: antialiased;color: #555;font-size: 1em;line-height: 1.5;cursor: auto;box-sizing: inherit;display: flex;position: fixed;z-index: 1060;top: 0;right: 0;bottom: 0;left: 0;flex-direction: row;justify-content: center;padding: .625em;overflow-x: hidden;transition: background-color .1s;-webkit-tap-highlight-color: transparent;transition:2s;background: rgba(0,0,0,.4);align-items: center;overflow-y: auto;"><div style="height: 200px; border-radius: 8px; width: 500px; background: white; color: lightgrey; transistion: font-weight: bolder;"><h1><center>'+title+'</center></h1><h1></h1><h1></h1><center><h2>'+msg+'</h2></center><button style="weight:50px; height: 50px;position: absolute;left: 658px;background: grey; float: left;" onclick="p();">OK</button></div></div>';
+r.innerHTML='<div style="-webkit-font-smoothing: antialiased;color: #555;font-size: 1em;line-height: 1.5;cursor: auto;box-sizing: inherit;display: flex;position: fixed;z-index: 1060;top: 0;right: 0;bottom: 0;left: 0;flex-direction: row;justify-content: center;padding: .625em;overflow-x: hidden;transition: background-color .1s;-webkit-tap-highlight-color: transparent;transition:2s;background: rgba(0,0,0,.4);align-items: center;overflow-y: auto;"><div style="height: 200px; border-radius: 8px; width: 500px; background: white; color: lightgrey; transistion: font-weight: bolder;"><h1><center>'+title+'</center></h1><h1></h1><h1></h1><center><h2>'+msg+'</h2></center><button style="weight:50px; height: 50px;position: absolute;left: 658px;background: grey; float: left;" onclick="Xtreme.p();">OK</button></div></div>';
 document.body.background='red';
 document.body.prepend(r);
  },
+ p: function p() {
+   r.style.transition = '2s';
+r.style.opacity='0'
+   setTimeout(function(){r.remove()}, 900);
+   setTimeout(function(){r.style.opacity=('1')}, 1000);
+},
  turnOnArcadeMode: function() {
   Runner.instance_.isArcadeMode = Runner.prototype.isArcadeMode;
 Runner.instance_.setArcadeMode = Runner.prototype.setArcadeMode;
@@ -211,16 +217,7 @@ usePackage: function(PackageName, include) { // Just do basicMiscs for now!
 },
    customCredits: function(creator, color, size, padding, backgroundcolor) {
     console.log('%cCreator: '+creator+'', 'color: '+color+'; font-size: '+size+'; padding: '+padding+'; background-color: '+backgroundcolor+';');
- }
-}
-Xtreme.Miscs.Runner=Runner;
-clear();
-/*
-   Made by: Lapide
-   For: Chrome Dino
-*/
-// Enjoy!
-var Xtreme = {
+ },
    help: function(withwhat) {
      console.log('Help commands:');
      console.log('usePackage: Helps you to use Xtreme.usePackage!');
@@ -709,6 +706,7 @@ clear();
 function l() {
   alert('Succses!');
 }
+Xtreme.Miscs.Runner=Runner;
 console.log('loadAssets has been found.');
 console.log('Ji.XtremeLoader added with yes.');
 console.log('Created TRUE');
